44c867b9985302cf1fc8d5d5fb0e556d5f83dfaf
Frank Carnevale <frank.carnevale@gmail.com>
2013-03-04 21:58:03 -0500

    Finish temporary landing page, and misc styling

--- codehabitinfo.md
+++ codehabitinfo.md
@@ -1,5 +1,8 @@
 x1. add twitter bootstrap
-2. temporary landing page
+x1a. add header nav bar
+x2. temporary landing page with "hero" box
 3. github auth
 4. basic profile section with gravatar and github profile info
-5. placeholder for
+5. placeholder for streak boxes
+6. placeholder for rightside feed
+7. post functionality - figure out how to format code within posts
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,27 +4,27 @@ describe "Pages" do
 
   describe "Home page" do
     
-    it "should have the content 'codehabit'" do
+    it "should have the content 'codehab.it'" do
       visit root_path
-      page.should have_content('codehabit')
+      page.should have_content('codehab.it')
     end
 
-    it "should have the title 'codehabit [home]'" do
+    it "should have the title 'codehab.it [home]'" do
       visit root_path
-      page.should have_selector('title', text: 'codehabit [home]')
+      page.should have_selector('title', text: 'codehab.it [home]')
     end
   end
 
   describe "About page" do
   	
-  	it "should have the content 'about codehabit'" do
+  	it "should have the content 'about codehab.it'" do
   	  visit about_path
-  	  page.should have_content('about codehabit')	
+  	  page.should have_content('about codehab.it')	
   	end
 
-  	it "should have the title 'codehabit [about]'" do
+  	it "should have the title 'codehab.it [about]'" do
   	  visit about_path
-  	  page.should have_selector('title', text: 'codehabit [about]')	
+  	  page.should have_selector('title', text: 'codehab.it [about]')	
   	end
   end
 end
